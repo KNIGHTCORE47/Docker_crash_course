4337214a683e77a6b918f2172bb1f2039e546c92
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-03-20 22:04:30 +0530

    feat: Implement basic Express server for multistage Docker setup
    - Created a new `server.js` file in the `06-Multistage_Docker` directory to serve as the entry point for the Node.js application.
    - Set up an Express server that listens on a specified port (defaulting to 3000) to handle incoming requests.
    - Implemented a simple GET endpoint at the root (`/`) that responds with a JSON object containing a success message:
      - Returns a message: "Hello World from Multistage_Docker_Node" to confirm the server is running.
    - Added logging to the console to indicate the server's running status and the port it is listening on.
    
    This commit establishes a foundational Express server that can be utilized in a multistage Docker build, allowing for further development and integration into a containerized environment.

---
+++ Docker (Udemy)/06-Multistage_Docker/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (requset, response) => {
+    response.json({
+        success: true,
+        message: 'Hello World from Multistage_Docker_Node'
+    }).status(200)
+})
+
+app.listen(port, function () {
+    console.log(`Server running on port http://localhost:${port}`)
+})
